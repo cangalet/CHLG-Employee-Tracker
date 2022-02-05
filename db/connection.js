d100b6dbc98f761dbd7f84695189602abbbbf3a8
Chris <cangalet@gmail.com>
2022-02-04 16:16:43 -0800

    pushing connection js to repo

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,14 @@
 const mysql = require('mysql2');
 const util = require('util');
+require('dotenv').config();
 
 // connect to the database
 const db = mysql.createConnection(
     {
         host: 'localhost',
-        user: 'root',
-        password: 'HappyHat420',
-        database: 'employee_tracker'
+        user: process.env.DB_USER,
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
     },
     console.log('Connected to the Employee Tracker database.')
 );
